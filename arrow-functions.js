90723dd00e695037c8c377464e71a0d851c1824b
Ergo Kannikka <ergokannikka@gmail.com>
2024-06-11 19:28:30 +0000

    code

--- arrow-functions.js
+++ arrow-functions.js
@@ -6,9 +6,17 @@
 // Vanilla JavaScript Function
 
 // Arrow Function With Parameters
-
+const addTwoNumbers = (a, b) => {
+    return a * b;
+}
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a, b) => a + b;
 // Implicit Returns
-
+const saySomething = message => console.log(message);
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    <p>`
+)
+console.log(returnMultipleLines());
